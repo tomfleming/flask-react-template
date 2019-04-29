9dd7f6dc19a99824b25a82b8ceefc4603c3f8d89
Tom Fleming <tomdfleming@gmail.com>
2019-04-28 22:03:33 -0700

    add redux to bootstrap. seems to be de facto standard for managing react state

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 npm install --save react react-dom @types/react @types/react-dom
+npm install --save redux react-redux @types/react-redux
+# UNCOMMENT FOR ENZYME TESTS
+# npm install -D enzyme @types/enzyme enzyme-adapter-react-16 @types/enzyme-adapter-react-16 react-test-renderer
+
 pipenv install --dev
